0b5cb20597523e09e97e7a4c5204f7c6d4436dfb
xianguo.zhu <xianguo.zhu@anmav.com>
2017-10-09 18:02:42 +0800

    add confuse

---
+++ KnowledgePoint/ZXGConfuse/codeObfuscation.h
@@ -0,0 +1,8 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Mon Oct  9 17:44:23 CST 2017
+#define modifyName HYzWYxZmkiiZCzKP
+#define _name ftrISGagkxqQUOOd
+#define modifyAge ssTpAsWZJqeRqzzN
+#define address yQTdZLpEeywsNfsW
+#endif
