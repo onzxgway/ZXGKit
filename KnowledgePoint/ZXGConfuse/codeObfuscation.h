06fb1abd36185c0fe5cef9eedd11879809638d3a
zhuxianguo529@163.com <huxianguo529@163.com>
2017-10-09 22:51:41 +0800

    add projectDemo

--- KnowledgePoint/ZXGConfuse/codeObfuscation.h
+++ KnowledgePoint/ZXGConfuse/codeObfuscation.h
@@ -1,7 +1,7 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Mon Oct  9 19:31:49 CST 2017
-#define XXX_testA atuQvxtATvEFQprj
-#define XXX_testB woZSIUXmVpnEUvvX
-#define XXX_testC JHtGUtQxAEGpzJWb
+//confuse string at Mon Oct  9 22:45:12 CST 2017
+#define XXX_testA OjALodTZGXoCMRZr
+#define XXX_testB JNHPmtkSVZhYfcBL
+#define XXX_testC puTyfscEUabNyfZm
 #endif
