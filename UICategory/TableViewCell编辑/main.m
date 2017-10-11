4761e3c5760b5d58712a13fd6b7f2d604c6edb6b
zhuxianguo529@163.com <huxianguo529@163.com>
2017-10-11 10:35:42 +0800

    add

---
+++ UICategory/TableViewCell编辑/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewCell编辑
+//
+//  Created by 朱献国 on 10/10/2017.
+//  Copyright © 2017 朱献国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
