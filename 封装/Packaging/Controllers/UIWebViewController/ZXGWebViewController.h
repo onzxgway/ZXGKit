ef024a6ca96d40e604f89a1dc43faeb63ab3fe0e
朱献国iMac <zhuxianguo529@163.com>
2018-04-18 10:32:37 +0800

    修改Xcode文件模板

---
+++ 封装/Packaging/Controllers/UIWebViewController/ZXGWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZXGWebViewController.h
+//  Packaging
+//
+//  Created by 朱献国 on 2018/4/18.
+//  Copyright © 2018年 朱献国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZXGWebViewController : UIViewController
+
+@end
---
+++ 封装/Packaging/Controllers/UIWebViewController/ZXGWebViewController.m
@@ -0,0 +1,39 @@
+//
+//  ZXGWebViewController.m
+//  封装
+//
+//  Created by 朱献国 on 2018/4/18.
+//  Copyright © 2018年 朱献国. All rights reserved.
+//
+
+#import "ZXGWebViewController.h"
+
+@interface ZXGWebViewController ()
+
+@end
+
+@implementation ZXGWebViewController
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+#pragma mark - CreateViews
+
+#pragma mark - Private
+
+#pragma mark - Public
+
+#pragma mark - LazyLoad
+
+#pragma mark - Network
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
