ea64e2c4e8d8e8590348315b7cc628d5f6796590
朱献国 <zhuxianguo529@163.com>
2018-04-23 18:14:30 +0800

    add

---
+++ 封装/CoreTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextDemo
+//
+//  Created by 朱献国 on 2018/4/23.
+//  Copyright © 2018年 朱献国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
