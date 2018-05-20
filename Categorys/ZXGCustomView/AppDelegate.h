dde76a53fa4583561bfe0c38bd0000e82fc497b5
zhuxianguo529@163.com <zhuxianguo529@163.com>
2018-05-20 14:42:52 +0800

    add

---
+++ Categorys/ZXGCustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXGCustomView
+//
+//  Created by 朱献国 on 2018/5/19.
+//  Copyright © 2018 朱献国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Categorys/ZXGCustomView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZXGCustomView
+//
+//  Created by 朱献国 on 2018/5/19.
+//  Copyright © 2018 朱献国. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
