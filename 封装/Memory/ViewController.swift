72a2a56b5cb0d35d851e72fcfdf7b8f9473fa2da
朱献国 <zhuxianguo529@163.com>
2018-10-08 18:14:48 +0800

    add

---
+++ 封装/Memory/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Memory
+//
+//  Created by 朱献国 on 2018/10/8.
+//  Copyright © 2018年 朱献国. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+// MARK: - 内存区域
+/**
+ 1.栈区 stack 存放的是局部变量或者实参 由系统进行管理的包括分配和回收等。
+ 
+ 2.堆区 heap 存放的是 alloc + init 创建的对象 由程序员手动管理，精确的申请内存，释放内存匹配是良好程序的基本要素。
+ 
+  比较：栈是系统数据结构，对应线程也是唯一的，优点是安全高效，缺点是使用数据不灵活必须［先进后出］
+       堆优点是使用数据灵活方便，适应面广泛，但是效率有一定降低。
+ 
+ 3.
+ */
+extension ViewController {
+    
+}
