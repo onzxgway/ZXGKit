ae6c2b721e0762de0cf28615ed47c43e36b9146b
朱献国 <zhuxianguo529@163.com>
2018-10-15 17:53:12 +0800

    add

---
+++ SenioriOS/SenioriOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SenioriOS
+//
+//  Created by 朱献国 on 2018/10/15.
+//  Copyright © 2018年 朱献国. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
