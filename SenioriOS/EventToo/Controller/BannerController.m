a705db0417f0b97371c0d8737de6bd1cd0ff559b
朱献国 <zhuxianguo529@163.com>
2018-11-20 17:49:13 +0800

    add

--- SenioriOS/EventToo/Controller/BannerController.m
+++ SenioriOS/EventToo/Controller/BannerController.m
@@ -18,13 +18,23 @@
 
 @implementation BannerController
 
+//- (void)loadView {
+//    self.view = [BannerView new];
+//}
+
+/**
+ 1. 轮播图片越界。
+ 2. 边界不能滑动。
+ 3. 导航控制器的 侧滑返回手势与b轮播图的左侧滑动手势冲突。
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
-    [self.view setClipsToBounds:YES];
+    [self.view setClipsToBounds:YES]; // 解决1
     
     [self createScrollView];
+    
 }
 
 - (void)createScrollView {
@@ -33,6 +43,7 @@
     
     BannerView *sc = [[BannerView alloc] initWithFrame:CGRectMake(30, 108, JZSCREEN_WIDTH - 2 * 30, (JZSCREEN_WIDTH - 2 * 35) * 0.5)];
     sc.backgroundColor = [UIColor whiteColor];
+    sc.showsHorizontalScrollIndicator = NO;
     sc.pagingEnabled = YES;
     sc.contentSize = CGSizeMake(CGRectGetWidth(sc.frame) * imgs.count, 0);
     sc.clipsToBounds = NO;
@@ -41,9 +52,13 @@
     
     for (NSInteger i = 0; i < imgs.count; i ++) {
         UIImageView *img = [[UIImageView alloc] initWithImage:[UIImage imageNamed:imgs[i]]];
+        img.userInteractionEnabled = YES;
         [sc addSubview:img];
         img.frame = CGRectMake(10 + CGRectGetWidth(sc.frame) * i, 0, CGRectGetWidth(sc.frame) - 10, CGRectGetHeight(sc.frame));
     }
+    
+    // 解决3
+    [self.navigationController.interactivePopGestureRecognizer requireGestureRecognizerToFail:sc.panGestureRecognizer];
     
 }
 
--- SenioriOS/EventToo/View/BannerView.m
+++ SenioriOS/EventToo/View/BannerView.m
@@ -10,32 +10,39 @@
 
 @implementation BannerView
 
-- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
-    NSLog(@"%s", __func__);
-    
-    CGRect frame = self.bounds;
-    newF = CGRectMake();
-}
+// 解决2
 
-//- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+//// 方式一
+//- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
 //    NSLog(@"%s", __func__);
 //
-//    UIView *targetV = nil;
-//    for (UIView *subView in [self.subviews.reverseObjectEnumerator allObjects]) {
+//    CGRect frame = self.bounds;
+//    frame = CGRectMake(CGRectGetMinX(frame) - 30, CGRectGetMinY(frame), CGRectGetMaxX(frame) + 60, CGRectGetHeight(frame));
 //
-//        CGPoint newP = [self convertPoint:point toView:subView];
-//        BOOL res = [subView pointInside:newP withEvent:event];
-//        if (res) {
-//            targetV = [subView hitTest:newP withEvent:event];
-//            break;
-//        }
-//    }
-//
-//    if (targetV == nil && [self pointInside:point withEvent:event]) {
-//        targetV = self;
-//    }
-//
-//    return targetV;
+//    return CGRectContainsPoint(frame, point);
 //}
+
+// 方式二
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    NSLog(@"%s", __func__);
+
+    UIView *targetV = nil;
+    for (UIView *v in [self.subviews.reverseObjectEnumerator allObjects]) {
+        
+        CGPoint newP = [self convertPoint:point toView:v];
+        BOOL res = [v pointInside:newP withEvent:event];
+        if (res) {
+            targetV = [v hitTest:newP withEvent:event];
+            break;
+        }
+        
+    }
+    
+    if (!targetV && [self pointInside:point withEvent:event]) {
+        targetV = self;
+    }
+    
+    return targetV;
+}
 
 @end
