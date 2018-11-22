420afa3179fc85b7f3f4b94d8645f3a6a56ce393
朱献国 <zhuxianguo529@163.com>
2018-11-22 09:34:49 +0800

    add

--- SenioriOS/EventToo/Controller/TGTestController.m
+++ SenioriOS/EventToo/Controller/TGTestController.m
@@ -24,6 +24,16 @@
 
 @implementation TGTestController
 
+/**
+ 总结：
+ 
+ 1.子控件的 内部的 touch事件在触发的时候，会向其父类方向，层层向上传递。（目前已知 ScrollView 会阻止控件内部的touch事件 继续向 父控件方向 传递）
+ 
+ 2.控件的 内部touch事件 与 外部添加的手势事件，没必然的联系。 内部的touch事件不响应，手势依旧可以触发。（手势触发与否与手势内部的touch事件和手势代理有关）
+ 
+ 3.不同层级的手势冲突发生在同种手势之间。
+ */
+
 - (void)loadView {
     self.view = [RootView new];
 }
@@ -48,6 +58,9 @@
     bl.frame = CGRectMake(92, 120, 148, 148);
     bl.backgroundColor = [UIColor blueColor];
     
+    /**
+     当 r 是 ScrollView 的时候，其本身的 UIScrollViewPanGestureRecognizer 手势与 其 父类的UIPanGestureRecognizer手势 并不冲突，
+     */
     BannerView *r = [BannerView new];
     [bl addSubview:r];
     r.frame = CGRectMake(10, 10, 128, 128);
@@ -56,6 +69,8 @@
     r.backgroundColor = [UIColor redColor];
     
     YellowView *y = [YellowView new];
+    [y addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(event1)]];
+    [self.view addSubview:y];
     [r addSubview:y];
     y.frame = CGRectMake(10, 10, 108, 108);
 //    [self.view addSubview:y];
@@ -64,7 +79,12 @@
 }
 
 - (void)event {
-    NSLog(@"event");
+    NSLog(@"BlueView event");
 }
+
+- (void)event1 {
+    NSLog(@"YellowView event");
+}
+
 
 @end
